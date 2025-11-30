c5a03637edb864dbcc60a5ad8dd1cdfff91e2c41
itsprinsprays <prncbntz@gmail.com>
2025-11-30 22:14:13 +0800

    huhuhu

--- src/main/java/SpringbootJDBC3/Service/SportCRUDService.java
+++ src/main/java/SpringbootJDBC3/Service/SportCRUDService.java
@@ -1,7 +1,11 @@
 package SpringbootJDBC3.Service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import SpringbootJDBC3.AthleteModel.Athlete;
+import SpringbootJDBC3.AthleteRepository.AthleteRepository;
 import SpringbootJDBC3.SportModel.Sport;
 import SpringbootJDBC3.SportRepository.SportRepository;
 
@@ -9,10 +13,14 @@ import SpringbootJDBC3.SportRepository.SportRepository;
 public class SportCRUDService {
 	
 	SportRepository sRepo;
+	AthleteRepository aRepo;
 	
-	public SportCRUDService(SportRepository sRepo) {
+	public SportCRUDService(SportRepository sRepo, AthleteRepository aRepo) {
 		this.sRepo = sRepo;
+		this.aRepo = aRepo;
 	}
+	
+	//Sport CRUD
 	
 	public String createSport(Sport sport) {
 		 Sport check = sRepo.getSportByID(sport.getSportID());
@@ -21,7 +29,62 @@ public class SportCRUDService {
 		 } 
 		sRepo.createSport(sport);
 		return "Sports Created ID : " + sport.getSportID();
-		
 	}
+	
+	public List<Sport> getAllSport() {
+		return sRepo.getAllSports();
+	}
+	
+	public Sport getSportByID(int ID) {
+		Sport check = sRepo.getSportByID(ID);
+		if(check == null) {
+			System.out.print("Sport ID : " + ID + " is not existing");
+		}
+		return check;
+	}
+	
+	public int updateSport(Sport sport) {
+		return sRepo.updateSport(sport);
+	}
+	
+	public int deleteSportByID(int ID) {
+		return sRepo.deleteSportByID(ID);
+	}
+	
+	
+	//Athlete CRUD
+	
+	public String createAthlete(Athlete athlete) {
+		Athlete check = aRepo.getAthleteByID(athlete.getSportID());
+		if(check != null) {
+			return ("Athlete ID : " + athlete.getSportID() + " is already existed");
+		}
+		aRepo.createAthlete(athlete);
+		return "Athlete ID : " + athlete.getSportID() + " created";
+	}
+	
+	public List<Athlete> getAllAthlete() {
+		return aRepo.getAllAthlete();
+	}
+	
+	public Athlete getAthleteByID(int ID) {
+		Athlete check = aRepo.getAthleteByID(ID);
+		if(check == null) {
+			System.out.print("Sport ID : " + ID + " is not existing");
+		}
+		return check;
+	}
+	
+	public int updateAthlete(Athlete athlete) {
+		return aRepo.updateAthlete(athlete);
+	}
+	
+	public int deleteAthleteByID(int ID) {
+		return aRepo.deleteAthleteByID(ID);
+	}
+	
+	
+	
+	
 
 }
