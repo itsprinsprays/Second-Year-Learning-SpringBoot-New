8acf74bea65ebb52d5eb54147fe9511971f9e40a
itsprinsprays <prncbntz@gmail.com>
2026-06-12 21:38:22 +0800

    Model Added

---
+++ src/main/java/SpringbootJPA2/Status.java
@@ -0,0 +1,6 @@
+package SpringbootJPA2;
+
+public enum Status {
+	ACTIVE,
+	INACTIVE
+}
---
+++ src/main/java/SpringbootJPA2/entity/Department.java
@@ -0,0 +1,44 @@
+package SpringbootJPA2.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import SpringbootJPA2.Status;
+
+@Entity
+@Table(name = "Product")
+public class Department {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int department_Id;
+	
+	@CreationTimestamp
+	private int created_at;
+	
+	@Column(nullable = false)
+	private String productName;
+	
+	@Column(nullable = false)
+	private String description;
+	
+	@Column(nullable = false)
+	private int stockQuantity;
+	
+	@Column(nullable = false) 
+	private String unit;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Status status = Status.ACTIVE;
+	
+	
+
+}
