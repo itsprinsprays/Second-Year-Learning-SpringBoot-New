6d38eb8f9681c1c36b82343923d13301a403282f
itsprinsprays <prncbntz@gmail.com>
2026-06-17 20:56:21 +0800

    Generate Refresh Token function added

--- src/main/java/SpringbootJPA1/security/JwtUtil.java
+++ src/main/java/SpringbootJPA1/security/JwtUtil.java
@@ -1,10 +1,14 @@
 package SpringbootJPA1.security;
 
+import java.util.Date;
+
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 
 @Component("util")
@@ -23,7 +27,33 @@ public class JwtUtil {
 	
 	@PostConstruct
 	public void init() {
-		this.SECRET = hmacShaKeysFor(secret.getBytes());
+		this.SECRET = Keys.hmacShaKeyFor(secret.getBytes());
+	}
+	
+	public String generateToken(String username) {
+		
+		return Jwts.builder()
+					.setSubject(username)
+					.claim("type", "access")
+					.setIssuedAt(new Date())
+					.setExpiration(new Date(
+							System.currentTimeMillis() + accessExpiration))
+					.signWith(SECRET)
+					.compact();
+		
+	}
+	
+	public String generateRefreshToken(String username) {
+		
+		return Jwts.builder()
+					.setSubject(username)
+					.claim("type", "refresh")
+					.setIssuedAt(new Date())
+					.setExpiration(new Date(
+							System.currentTimeMillis() + refreshExpiration))
+					.signWith(SECRET)
+					.compact();
+		
 	}
 	
 }
